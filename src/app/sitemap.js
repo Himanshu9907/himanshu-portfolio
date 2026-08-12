fc5be74e5cb6aa039f1cd66f7a6ed2975d666fcc
himanshu9907 <himanshu05119@gmail.com>
2026-08-12 13:42:16 +0530

    Add Google Search Console verification

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,6 +1,62 @@
-const baseUrl = "https://your-portfolio.vercel.app";
+// const baseUrl = "https://your-portfolio.vercel.app";
+
+// export default function sitemap() {
+//   return [
+//     {
+//       url: baseUrl,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 1,
+//     },
+
+//     {
+//       url: `${baseUrl}/projects`,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.9,
+//     },
+
+//     {
+//       url: `${baseUrl}/#about`,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.7,
+//     },
+
+//     {
+//       url: `${baseUrl}/#services`,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.7,
+//     },
+
+//     {
+//       url: `${baseUrl}/#contact`,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 0.8,
+//     },
+//   ];
+// }
+
+
+// const baseUrl = "https://himanshu-portfolio-tawny.vercel.app";
+
+// export default function sitemap() {
+//   return [
+//     {
+//       url: baseUrl,
+//       lastModified: new Date(),
+//       changeFrequency: "monthly",
+//       priority: 1,
+//     },
+//   ];
+// }
+
 
 export default function sitemap() {
+  const baseUrl = "https://himanshu-portfolio-tawny.vercel.app";
+
   return [
     {
       url: baseUrl,
@@ -8,33 +64,11 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 1,
     },
-
     {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
-    },
-
-    {
-      url: `${baseUrl}/#about`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-
-    {
-      url: `${baseUrl}/#services`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-
-    {
-      url: `${baseUrl}/#contact`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
   ];
 }
